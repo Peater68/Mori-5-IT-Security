a6d4bd92133bd19002ca511dd2db80b88ede8770
Marogna Péter <prenzo97@gmail.com>
2020-11-14 02:06:35 +0100

    Add login and register screens with logic

--- mobile/app/src/main/java/hu/bme/caffshare/di/ViewModelModule.kt
+++ mobile/app/src/main/java/hu/bme/caffshare/di/ViewModelModule.kt
@@ -6,6 +6,8 @@ import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 import hu.bme.caffshare.ui.cafflist.CaffListViewModel
+import hu.bme.caffshare.ui.login.LoginViewModel
+import hu.bme.caffshare.ui.register.RegisterViewModel
 
 @Suppress("unused")
 @Module
@@ -16,4 +18,13 @@ abstract class ViewModelModule {
     @ViewModelKey(CaffListViewModel::class)
     abstract fun bindBlankViewModel(caffListViewModel: CaffListViewModel): ViewModel
 
+    @Binds
+    @IntoMap
+    @ViewModelKey(LoginViewModel::class)
+    abstract fun bindLoginViewModel(loginViewModel: LoginViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(RegisterViewModel::class)
+    abstract fun bindRegisterViewModel(registerViewModel: RegisterViewModel): ViewModel
 }
---
+++ mobile/app/src/main/java/hu/bme/caffshare/ui/login/LoginFragment.kt
@@ -0,0 +1,63 @@
+package hu.bme.caffshare.ui.login
+
+import android.os.Bundle
+import android.view.View
+import co.zsmb.rainbowcake.base.OneShotEvent
+import co.zsmb.rainbowcake.base.RainbowCakeFragment
+import co.zsmb.rainbowcake.dagger.getViewModelFromFactory
+import co.zsmb.rainbowcake.navigation.navigator
+import com.google.android.material.snackbar.Snackbar
+import hu.bme.caffshare.R
+import hu.bme.caffshare.ui.cafflist.CaffListFragment
+import hu.bme.caffshare.ui.register.RegisterFragment
+import hu.bme.caffshare.util.isNotEmpty
+import hu.bme.caffshare.util.showErrorSnackBar
+import hu.bme.caffshare.util.text
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : RainbowCakeFragment<LoginViewState, LoginViewModel>() {
+
+    override fun provideViewModel() = getViewModelFromFactory()
+    override fun getViewResource() = R.layout.fragment_login
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRegisterButton()
+        setupLoginButton()
+        setupPasswordForgotButton()
+    }
+
+    private fun setupRegisterButton() {
+        registerButton.setOnClickListener {
+            navigator?.replace(RegisterFragment())
+        }
+    }
+
+    private fun setupLoginButton() {
+        loginButton.setOnClickListener {
+            if (usernameInput.isNotEmpty() && passwordInput.isNotEmpty()) {
+                viewModel.login(usernameInput.text, passwordInput.text)
+            }
+        }
+    }
+
+    private fun setupPasswordForgotButton() {
+        forgotPasswordButton.setOnClickListener {
+            Snackbar.make(requireView(), "Not implemented", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun render(viewState: LoginViewState) = Unit
+
+    override fun onEvent(event: OneShotEvent) {
+        when (event) {
+            is LoginViewModel.LoginSuccessful -> {
+                navigator?.replace(CaffListFragment())
+            }
+            is LoginViewModel.LoginFailed -> {
+                showErrorSnackBar("Wrong username or password!")
+            }
+        }
+    }
+}
