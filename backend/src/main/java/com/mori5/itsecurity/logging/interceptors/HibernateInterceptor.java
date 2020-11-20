1e4655400b0cdf75312749597e545de49d9f5a76
Laki Dávid <lakidavid97@gmail.com>
2020-11-20 23:46:19 +0100

    Fix jpa logging

--- backend/src/main/java/com/mori5/itsecurity/logging/interceptors/HibernateInterceptor.java
+++ backend/src/main/java/com/mori5/itsecurity/logging/interceptors/HibernateInterceptor.java
@@ -1,7 +1,7 @@
 package com.mori5.itsecurity.logging.interceptors;
 
+import com.mori5.itsecurity.configuration.SpringContextUtil;
 import com.mori5.itsecurity.logging.service.LoggingService;
-import lombok.RequiredArgsConstructor;
 import org.hibernate.EmptyInterceptor;
 import org.hibernate.type.Type;
 import org.springframework.stereotype.Component;
@@ -9,13 +9,14 @@ import org.springframework.stereotype.Component;
 import java.io.Serializable;
 
 @Component
-@RequiredArgsConstructor
 public class HibernateInterceptor extends EmptyInterceptor {
 
-    private final LoggingService loggingService;
+    private LoggingService loggingService = null;
 
     @Override
     public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+        loadLoggingServiceIfNeeded();
+
         loggingService.logSave(entity);
 
         return super.onSave(entity, id, state, propertyNames, types);
@@ -23,9 +24,19 @@ public class HibernateInterceptor extends EmptyInterceptor {
 
     @Override
     public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+        loadLoggingServiceIfNeeded();
+
         loggingService.logDeleting(entity);
 
         super.onDelete(entity, id, state, propertyNames, types);
+    }
+
+    private void loadLoggingServiceIfNeeded() {
+        if (loggingService != null) {
+            return;
+        }
+
+        loggingService = SpringContextUtil.getApplicationContext().getBean(LoggingService.class);
     }
 
 }
