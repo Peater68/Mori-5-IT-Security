e1bff6df7f40468235507b51bd2d32697c0adc50
Marogna Péter <prenzo97@gmail.com>
2020-11-23 12:39:03 +0100

    Add interceptor for handling refresh token

---
+++ mobile/app/src/main/java/hu/bme/caffshare/data/network/interceptor/RefreshTokenInterceptor.kt
@@ -0,0 +1,63 @@
+package hu.bme.caffshare.data.network.interceptor
+
+import com.squareup.moshi.Moshi
+import hu.bme.caffshare.data.local.TokenDataSource
+import hu.bme.caffshare.data.network.NetworkModule
+import hu.bme.caffshare.data.network.model.RefreshTokenRequestDTO
+import hu.bme.caffshare.data.network.model.TokensDTO
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import timber.log.Timber
+
+class RefreshTokenInterceptor(
+    private val tokenDataSource: TokenDataSource,
+    private val moshi: Moshi
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val interceptedRequest = chain.request()
+        val interceptedResponse = chain.proceed(interceptedRequest)
+
+        if (interceptedResponse.code == 401) {
+            val refreshToken = tokenDataSource.refreshToken
+
+            if (refreshToken != null) {
+                val accessTokenRequest = buildAccessTokenRequest(refreshToken)
+                val accessTokenResponse = chain.proceed(accessTokenRequest)
+
+                if (accessTokenResponse.code == 200) {
+                    val bodyString = accessTokenResponse.body!!.string()
+                    val tokensDTO = moshi.adapter(TokensDTO::class.java).fromJson(bodyString)!!
+
+                    tokenDataSource.accessToken = tokensDTO.accessToken
+                    tokenDataSource.refreshToken = tokensDTO.refreshToken
+
+                    return chain.proceed(interceptedRequest.newBuilder().build())
+                } else {
+                    Timber.e("Error while retrieving access token with refresh token!")
+                }
+            } else {
+                Timber.e("No refresh token found!")
+            }
+        }
+        return interceptedResponse
+    }
+
+    private fun buildAccessTokenRequest(refreshToken: String): Request {
+        val endpoint = NetworkModule.BASE_URL + "/api/auth/token"
+
+        val content = moshi
+            .adapter(RefreshTokenRequestDTO::class.java)
+            .toJson(RefreshTokenRequestDTO(refreshToken))
+            .toByteArray()
+
+        val body = content.toRequestBody("application/json".toMediaTypeOrNull(), 0, content.size)
+
+        return Request.Builder()
+            .url(endpoint)
+            .method("POST", body)
+            .build()
+    }
+}
