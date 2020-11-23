fc2616286cec12941d2323a35b6f554aa046c273
Marogna Péter <prenzo97@gmail.com>
2020-11-23 12:38:50 +0100

    Add interceptor for handling access token

---
+++ mobile/app/src/main/java/hu/bme/caffshare/data/network/interceptor/AccessTokenInterceptor.kt
@@ -0,0 +1,20 @@
+package hu.bme.caffshare.data.network.interceptor
+
+import hu.bme.caffshare.data.local.TokenDataSource
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AccessTokenInterceptor(private val tokenDataSource: TokenDataSource) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+
+        val newRequest =
+            tokenDataSource.accessToken?.let { accessToken ->
+                request.newBuilder()
+                    .addHeader(TokenDataSource.ACCESS_TOKEN_HEADER, accessToken)
+                    .build()
+            } ?: request
+
+        return chain.proceed(newRequest)
+    }
+}
