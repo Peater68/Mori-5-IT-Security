d8a96e76752e1bf535bce55b50ebb8d9b4efd842
Marogna Péter <prenzo97@gmail.com>
2020-11-27 22:28:46 +0100

    Add missing calls to CaffInteractor

--- mobile/app/src/main/java/hu/bme/caffshare/domain/CaffInteractor.kt
+++ mobile/app/src/main/java/hu/bme/caffshare/domain/CaffInteractor.kt
@@ -1,6 +1,28 @@
 package hu.bme.caffshare.domain
 
+import android.net.Uri
+import hu.bme.caffshare.data.network.NetworkDataSource
+import hu.bme.caffshare.domain.model.DomainCaffDetails
+import hu.bme.caffshare.domain.model.DomainCaffSum
 import javax.inject.Inject
 
-class CaffInteractor @Inject constructor() {
+class CaffInteractor @Inject constructor(
+    private val networkDataSource: NetworkDataSource
+) {
+    suspend fun deleteCaffById(caffId: String): Boolean = networkDataSource.deleteCaffById(caffId)
+
+    // TODO: handle downloaded byte stream
+    suspend fun downloadCaffFile(caffId: String): Boolean {
+        val caffFileInputStream = networkDataSource.downloadCaffFile(caffId) ?: return false
+
+        return false
+    }
+
+    suspend fun getCaffFiles(): List<DomainCaffSum>? = networkDataSource.getCaffFiles()
+
+    suspend fun getCaffFileDetails(caffId: String): DomainCaffDetails? =
+        networkDataSource.getCaffFileDetails(caffId)
+
+    suspend fun uploadCaffFile(caffFileUri: Uri): Boolean =
+        networkDataSource.uploadCaffFile(caffFileUri)
 }
