c12320d15fd18c03a0b92164d7c5edfd864f18ef
Marogna Péter <prenzo97@gmail.com>
2020-11-27 22:34:59 +0100

    Add missing calls to UserInteractor

--- mobile/app/src/main/java/hu/bme/caffshare/domain/UserInteractor.kt
+++ mobile/app/src/main/java/hu/bme/caffshare/domain/UserInteractor.kt
@@ -7,7 +7,26 @@ import javax.inject.Inject
 class UserInteractor @Inject constructor(
     private val networkDataSource: NetworkDataSource
 ) {
-    suspend fun getCurrentUserProfile(): DomainUser? {
-        return networkDataSource.getCurrentUserProfile()
-    }
+
+    suspend fun getCurrentUserProfile(): DomainUser? = networkDataSource.getCurrentUserProfile()
+
+    suspend fun banUser(userId: String): Boolean = networkDataSource.banUser(userId)
+
+    suspend fun deleteCurrentUser(): Boolean = networkDataSource.deleteCurrentUser()
+
+    suspend fun getAllUsers(): List<DomainUser>? = networkDataSource.getAllUsers()
+
+    suspend fun makeUserAdmin(userId: String): Boolean = networkDataSource.makeUserAdmin(userId)
+
+    suspend fun updateCurrentUser(
+        firstName: String,
+        lastName: String,
+        username: String,
+        email: String,
+    ): Boolean = networkDataSource.updateCurrentUser(
+        firstName = firstName,
+        lastName = lastName,
+        username = username,
+        email = email,
+    )
 }
