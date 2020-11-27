a4fa0553c456e4faf33def181c2328efb927499f
Marogna Péter <prenzo97@gmail.com>
2020-11-27 22:31:40 +0100

    Add missing calls to CommentInteractor

--- mobile/app/src/main/java/hu/bme/caffshare/domain/CommentInteractor.kt
+++ mobile/app/src/main/java/hu/bme/caffshare/domain/CommentInteractor.kt
@@ -1,6 +1,18 @@
 package hu.bme.caffshare.domain
 
+import hu.bme.caffshare.data.network.NetworkDataSource
+import hu.bme.caffshare.domain.model.DomainComment
 import javax.inject.Inject
 
-class CommentInteractor @Inject constructor() {
+class CommentInteractor @Inject constructor(
+    private val networkDataSource: NetworkDataSource
+) {
+    suspend fun deleteComment(commentId: String): Boolean =
+        networkDataSource.deleteComment(commentId)
+
+    suspend fun getCommentsForCaff(caffId: String): List<DomainComment>? =
+        networkDataSource.getCommentsForCaff(caffId)
+
+    suspend fun addComment(caffId: String, comment: String): Boolean =
+        networkDataSource.addComment(caffId, comment)
 }
