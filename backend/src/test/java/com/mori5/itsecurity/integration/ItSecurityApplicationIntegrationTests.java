e1c7f6942800d52c91eea976f455a886d19cc71b
Laki Dávid <lakidavid97@gmail.com>
2020-11-28 18:41:10 +0100

    Add integration tests of tag endpoints

---
+++ backend/src/test/java/com/mori5/itsecurity/integration/ItSecurityApplicationIntegrationTests.java
@@ -0,0 +1,87 @@
+package com.mori5.itsecurity.integration;
+
+import com.mori5.itsecurity.domain.Role;
+import com.mori5.itsecurity.domain.User;
+import com.mori5.itsecurity.repository.TagRepository;
+import com.mori5.itsecurity.repository.UserRepository;
+import com.mori5.itsecurity.service.TokenService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ItSecurityApplicationIntegrationTests {
+
+    @LocalServerPort
+    protected int randomServerPort;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @MockBean
+    protected TagRepository tagRepository;
+    @MockBean
+    protected UserRepository userRepository;
+
+    protected static User adminUser = User.builder()
+            .id("e497255a-9f4c-42a0-83db-abc3ce6c34a4")
+            .username("admin")
+            .firstName("Admin")
+            .lastName("Test")
+            .password("notHashedPwd")
+            .role(Role.ADMIN)
+            .isBanned(false)
+            .build();
+    protected static User customerUser = User.builder()
+            .id("c497255a-9f4c-42a0-83db-abc3ce6c34a4")
+            .username("customer")
+            .firstName("Customer")
+            .lastName("Test")
+            .password("notHashedPwd")
+            .role(Role.CUSTOMER)
+            .isBanned(false)
+            .build();
+
+    protected WebTestClient client;
+
+    @BeforeEach
+    private void setupDefaults() {
+        when(userRepository.findByUsername("admin")).thenReturn(
+                Optional.of(
+                        adminUser
+                )
+        );
+        when(userRepository.findByUsername("customer")).thenReturn(
+                Optional.of(
+                        adminUser
+                )
+        );
+        when(userRepository.findAll()).thenReturn(
+                List.of(adminUser, customerUser)
+        );
+    }
+
+    protected String getAccessTokenFor(User user) {
+        return tokenService.generateAccessToken(user.getUsername(), user.getId(), user.getRole());
+    }
+
+    protected void initWebApiClientWithToken(String token) {
+        client = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + randomServerPort + "/api")
+                .responseTimeout(Duration.ofMinutes(1))
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessTokenFor(adminUser))
+                .build();
+    }
+
+}
---
+++ backend/src/test/java/com/mori5/itsecurity/integration/TagIntegrationTests.java
@@ -0,0 +1,51 @@
+package com.mori5.itsecurity.integration;
+
+import com.mori5.itsecurity.domain.Tag;
+import com.mori5.itsecurity.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TagIntegrationTests extends ItSecurityApplicationIntegrationTests {
+
+    @BeforeEach
+    void setup() {
+        when(tagRepository.findAll()).thenReturn(
+                new ArrayList<>(
+                        Collections.singletonList(
+                                Tag.builder()
+                                        .id("f497255a-9f4c-42a0-83db-abc3ce6c34a4")
+                                        .title("TEST_TITLE")
+                                        .build()
+                        )
+                )
+        );
+
+        initWebApiClientWithToken(getAccessTokenFor(adminUser));
+    }
+
+    @Test
+    void testGetTags() {
+        client.get().uri("/tags")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$[0].id").isEqualTo("f497255a-9f4c-42a0-83db-abc3ce6c34a4")
+                .jsonPath("$[0].title").isEqualTo("TEST_TITLE");
+    }
+
+}
