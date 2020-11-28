5a575ac3e65f62907dd98967cb44f8adc761f620
Laki Dávid <lakidavid97@gmail.com>
2020-11-28 21:51:36 +0100

    Add user integration tests

---
+++ backend/src/test/java/com/mori5/itsecurity/integration/UserIntegrationTests.java
@@ -0,0 +1,31 @@
+package com.mori5.itsecurity.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+public class UserIntegrationTests extends ItSecurityApplicationIntegrationTests {
+
+    @Test
+    void testGetUsersAsAdmin() {
+        initWebApiClientWithToken(getAccessTokenFor(adminUser));
+
+        client.get().uri("/users")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$[0].id").isEqualTo(adminUser.getId())
+                .jsonPath("$[1].id").isEqualTo(customerUser.getId())
+                .jsonPath("$[3].id").doesNotExist();
+    }
+
+    @Test
+    void testGetUsersAsCustomer() {
+        initWebApiClientWithToken(getAccessTokenFor(customerUser));
+
+        client.get().uri("/users")
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+}
